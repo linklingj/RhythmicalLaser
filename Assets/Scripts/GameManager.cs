09ed764f18f7638dd85697ed530e71ea0c2904a7
linklingj <jaehyunc413@gmail.com>
2022-10-05 17:16:21 +0900

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum GameState {
+    Title,
+    Menu,
+    Play,
+    Result
+}
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public GameState State;
+    public static event Action<GameState> OnGameStateChange;
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+    
+    public void UpdateGameState(GameState newState) {
+        State = newState;
+        switch(newState) {
+            case GameState.Title:
+                break;
+            case GameState.Menu:
+                break;
+            case GameState.Play:
+                break;
+            case GameState.Result:
+                break;
+            default:
+                break;
+        }
+        OnGameStateChange?.Invoke(newState);
+    }
+}
