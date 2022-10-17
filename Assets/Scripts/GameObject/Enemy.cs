9281a1339b331d69df6977fddcf8cd5541384d50
linklingj <jaehyunc413@gmail.com>
2022-10-17 16:34:50 +0900

    enemy setting

--- Assets/Scripts/GameObject/Enemy.cs
+++ Assets/Scripts/GameObject/Enemy.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     GameObject player;
+    Rigidbody2D rb;
     public int hp;
+    float turnSmoothVelocity;
 
     private void Start() {
         player = GameObject.FindGameObjectsWithTag("Player")[0];
+        rb = GetComponent<Rigidbody2D>();
     }
     public void Die() {
         Destroy(gameObject);
@@ -16,9 +19,12 @@ public class Enemy : MonoBehaviour
     public void TurnToward(float turnSmoothTime) {
         Vector2 lookDir = player.transform.position - transform.position;
         float targetAngle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        float angle = Mathf.SmoothDampAngle(transform.localEulerAngles.z, targetAngle, ref turnSmoothVelocity, Time.smoothDeltaTime * turnSmoothTime);
+
+        rb.rotation = angle;
     }
 
     public void MoveToward(float speed) {
-
+        transform.Translate(Vector3.right * speed);
     }
 }
